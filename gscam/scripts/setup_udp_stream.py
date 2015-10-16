b28eba85dd885db497bd9386875da18ae48a6a99
Paul Ruvolo <paullundyruvolo@gmail.com>
2015-10-16 09:50:56 -0400

    added video mode configurability

--- gscam/scripts/setup_udp_stream.py
+++ gscam/scripts/setup_udp_stream.py
@@ -8,11 +8,17 @@ rospy.init_node('setup_udp_stream')
 
 receive_port = rospy.get_param('~receive_port')
 host = rospy.get_param('~host')
+width = rospy.get_param('~width')
+height = rospy.get_param('~height')
+fps = rospy.get_param('~fps')
+
+video_mode = "-ex sports -awb off -mm matrix -w " + str(width) + " -h " + str(height) + " -fps " + str(fps) + " -b 2000000"
+
 port = 10003
 size = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-s.send(str(receive_port) + "\n")
+s.send(str(receive_port) + "," + str(video_mode) +"\n")
 all_data = ""
 while not all_data.endswith('\n'):
 	data = s.recv(size)
